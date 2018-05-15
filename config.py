cf48cdf4bb17249ebcf71db0865ba7a6184aab98
gauri <gauri@pratilipi.com>
2018-05-15 19:14:15 +0530

    [modify] changed config

--- config.py
+++ config.py
@@ -1,8 +1,19 @@
 import os
 
+"""
+|--------|---|--------|---|---|
+|threads |cpu|mem_resv|mem|tps|
+|--------|---|--------|---|---|
+|100+100 | 64|     128|256| 50|
+|10+10   | 64|     128|256|150|
+|10+10   |128|     256|512| 40|
+|100+100 |128|     256|512| 75|
+|50+50   | 64|     128|256|125|
+|--------|---|--------|---|---|
+"""
 # no. of parallel threads
-NO_OF_READ_THREADS = 50
-NO_OF_PROCESS_THREADS = 50
+NO_OF_READ_THREADS = 10
+NO_OF_PROCESS_THREADS = 10
 
 # sqs 
 AWS_REGION = 'ap-south-1'
